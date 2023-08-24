57b794ac7737d98d5f058d9c8d8fcee69a7f41c7
Valéria Cavalcanti <valeria.cavalcanti@ifpb.edu.br>
2023-08-24 12:41:34 -0300

    Atividade 01

--- atividades/01/1014.py
+++ atividades/01/1014.py
@@ -1,6 +1,6 @@
 distancia = int(input("Distância: "))
 combustivel = float(input("Quantidade de combustível: "))
 
-consumo = distancia / litcombustivelros
+consumo = distancia / combustivel
 
 print('{:.3f} km/l'.format(consumo))
