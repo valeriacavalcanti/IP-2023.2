14e6d2a7eea191b7286cf7ae8fe9a7f9e527ee74
Valéria Cavalcanti <valeria.cavalcanti@ifpb.edu.br>
2023-08-24 12:26:46 -0300

    Aula 24/08/2023

---
+++ aulas/20230824/exemplo01.py
@@ -0,0 +1,12 @@
+n1 = int(input("Primeira nota: "))
+n2 = int(input("Segunda nota: "))
+n3 = int(input("Terceira nota: "))
+n4 = int(input("Quarta nota: "))
+
+media = (n1*2+n2*4+n3*6+n4*8)/20
+
+print('Média ponderada =',media)
+print('Nota 1:',n1, end='#')
+print('Nota 2:', n2, end='#')
+print('Nota 3:',n3, end='#')
+print('Nota 4:', n4, end='#')
