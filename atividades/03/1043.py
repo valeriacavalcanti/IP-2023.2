f4f4994d851d21c6c1505442095a12fdda688b2e
Valéria Cavalcanti <valeria.cavalcanti@ifpb.edu.br>
2023-09-11 20:08:29 -0300

    Atividade 03

--- atividades/03/1043.py
+++ atividades/03/1043.py
@@ -2,14 +2,6 @@ l1 = float(input('Lado 1: '))
 l2 = float(input('Lado 2: '))
 l3 = float(input('Lado 3: '))
 
-"""
-  Lembre que, se o salário for R$ 3002.00, 
-  a taxa que incide é de 8% apenas sobre R$ 1000.00, 
-  pois a faixa de salário que fica de R$ 0.00 até R$ 2000.00 é isenta de Imposto de Renda. 
-  No exemplo fornecido (abaixo), a taxa é de 8% sobre R$ 1000.00 + 18% sobre R$ 2.00, 
-  o que resulta em R$ 80.36 no total. 
-"""
-
 if (l1 > l2) and (l1 > l3):
   maior = l1
   soma = l2 + l3
--- atividades/03/1051.py
+++ atividades/03/1051.py
@@ -1,5 +1,13 @@
 salario = float(input("Salário: "))
 
+"""
+  Lembre que, se o salário for R$ 3002.00, 
+  a taxa que incide é de 8% apenas sobre R$ 1000.00, 
+  pois a faixa de salário que fica de R$ 0.00 até R$ 2000.00 é isenta de Imposto de Renda. 
+  No exemplo fornecido (abaixo), a taxa é de 8% sobre R$ 1000.00 + 18% sobre R$ 2.00, 
+  o que resulta em R$ 80.36 no total. 
+"""
+
 if (salario <= 2000):
     ir = 0                          # faixa 01
 elif (salario <= 3000):
